2b28d20bd142de4508077ed39d88a656b28abf7f
ALiangGod <128807425+ALiangGod@users.noreply.github.com>
2023-04-06 20:56:07 +0800

    Add files via upload

---
+++ Date23_4_6.cpp
@@ -0,0 +1,27 @@
+/*#include<stdio.h>
+int main()
+{
+	long long i,a,b,n;
+	scanf("%lld%lld%lld",&a,&b,&n);
+	long long fate[8] = {0,a,a,a,a,a,b,b};
+	long long n7 = 5*a+2*b;
+	long long k = 7*(n/n7);
+	n = n%n7;
+	for(i=1;n>0;i++)
+	{
+		n -= fate[i];
+		k++;
+	}
+	printf("%lld",k);
+	return 0;
+}*/
+#include<stdio.h>
+int main()
+{
+	int i,j,k;
+	char str[27] = "abcdefjhigklmnopqrstuvwxyz";
+	for(i=0;i<27;i++)
+	{
+		for()
+	}
+}
