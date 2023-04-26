75966f157e8c2ea3fcea94490a009e9fd2e9831b
ALiangGod <128807425+ALiangGod@users.noreply.github.com>
2023-04-26 17:04:35 +0800

    Add files via upload

---
+++ Date23_4_26_ArrayListTest.java
@@ -0,0 +1,40 @@
+package myPackage;
+import java.util.Scanner;
+import java.util.ArrayList;
+public class Date23_4_26_ArrayListTest {
+    public static void main(String[] args) {
+//        ArrayList<Integer> al = new ArrayList<>();
+//        al.add("aaa");
+//        al.add("bbb");
+//        al.add("ccc");
+//        al.add(1);
+//        al.add(2);
+//        ArrayList<Date23_4_26_StudentClass> al = new ArrayList<>();
+//        Date23_4_26_StudentClass s1 = new Date23_4_26_StudentClass("wang", 18, "tenClass");
+//        al.add(s1);
+//        Date23_4_26_StudentClass stu = new Date23_4_26_StudentClass();
+//        stu = al.get(0);
+//        System.out.println(al.get(0).getName());
+//        for (int i = 0; i < al.size(); i++) {
+//            System.out.print(al.get(i));
+//        }
+//        ArrayList<Date23_4_26_StudentClass> list = new ArrayList<>();
+//        Date23_4_26_StudentClass s1 = new Date23_4_26_StudentClass();
+//        Date23_4_26_StudentClass s2 = new Date23_4_26_StudentClass();
+//        Scanner sc = new Scanner(System.in);
+//        s1.setName(sc.next());
+//        s1.setAge(sc.nextInt());
+//        s1.setS_class(sc.next());
+//        s2.setName(sc.next());
+//        s2.setAge(sc.nextInt());
+//        s2.setS_class(sc.next());
+//        list.add(s1);
+//        list.add(s2);
+//        System.out.println(list.get(0).getAge());
+//        System.out.println(list.get(1).getAge());
+
+    }
+}
+
+
+
---
+++ Date23_4_26_StudentClass.java
@@ -0,0 +1,37 @@
+package myPackage;
+
+public class Date23_4_26_StudentClass {
+    private String name;
+    private int age;
+    private String s_class;
+    public Date23_4_26_StudentClass(){}
+    public Date23_4_26_StudentClass(String name, int age, String s_class){
+        this.name = name;
+        this.age = age;
+        this.s_class = s_class;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getS_class() {
+        return s_class;
+    }
+
+    public void setS_class(String s_class) {
+        this.s_class = s_class;
+    }
+}
